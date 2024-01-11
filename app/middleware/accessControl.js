39e9b0033de563914d226ebba3b78a18a81e9faf
jiangyanxi <jiangyanxi@live.com>
2024-01-11 16:26:00 +0800

    优化

--- app/middleware/accessControl.js
+++ app/middleware/accessControl.js
@@ -9,25 +9,32 @@
 const _ = require("lodash");
 
 module.exports = (options, app) => {
+  const { enable } = app.config.jwt;
+
   return async function accessControl(ctx, next) {
-    let userInfo;
-    if (ctx.state.user && ctx.state.user.uuid) {
-      userInfo = await app.cache.default.wrap(ctx.state.user.uuid, async () => {
-        return null; // 查询用户信息
-      });
-    }
+    if (enable) {
+      let userInfo;
+      if (ctx.state.user && ctx.state.user.uuid) {
+        userInfo = await app.cache.default.wrap(
+          ctx.state.user.uuid,
+          async () => {
+            return null; // 查询用户信息
+          }
+        );
+      }
 
-    if (!userInfo) {
-      throw new app.jwt.UnauthorizedError("unauthorized", {
-        message: "unauthorized",
-      });
-    }
+      if (!userInfo) {
+        throw new app.jwt.UnauthorizedError("unauthorized", {
+          message: "unauthorized",
+        });
+      }
 
-    if ("forbidden" == userInfo.status) {
-      // 封号
-      throw new app.jwt.UnauthorizedError("forbidden", {
-        message: "forbidden",
-      });
+      if ("forbidden" == userInfo.status) {
+        // 封号
+        throw new app.jwt.UnauthorizedError("forbidden", {
+          message: "forbidden",
+        });
+      }
     }
 
     await next();
